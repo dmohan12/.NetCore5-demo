54413fadd747bc563ed53a0dbcc602f6a3f23a35
Damon Mohan <damon.mohan@credera.com>
2021-02-19 12:10:57 -0600

    added delete and edit operations for category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -46,6 +46,74 @@ namespace damon_mvc.Controllers
 
         }
 
+         //Edit - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if(ModelState.IsValid){
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+             return RedirectToAction("Index");
+
+            }
+
+            return View(obj);
+
+        }
+
+        //GET - Edit view
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+           //Post - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+                _db.Category.Remove(obj);
+                _db.SaveChanges();
+             return RedirectToAction("Index");
+
+        }
+
+           //GET - Edit view
+        public IActionResult Delete(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+
      
     }
 }
